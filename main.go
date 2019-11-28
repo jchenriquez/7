8bb693bcc7069ff54f27d0b24b754bae1e1456cc
jchenriquez <jc.henriquez04@gmail.com>
2019-11-28 18:25:17 -0500

    Adding implementation for negatives

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 
 func reverse(x int) int {
 	var res int
+	sign := x / int(math.Abs(float64(x)))
+	x = int(math.Abs(float64(x)))
 	tens := int(math.Pow(10, math.Floor(math.Log10(float64(x)))))
 
 	for x > 0 {
@@ -17,9 +19,9 @@ func reverse(x int) int {
 		tens/=10
 	}
 
-	return res
+	return res*sign
 }
 
 func main() {
-	fmt.Printf("result %d\n", reverse(1269))
+	fmt.Printf("result %d\n", reverse(-1269))
 }
