873f431b36ba564ca6ae258c58669512377b88e5
jchenriquez <jc.henriquez04@gmail.com>
2019-11-28 18:26:31 -0500

    adding solution for edge case.

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 
 func reverse(x int) int {
 	var res int
+	if x == 0 {
+		return 0
+	}
 	sign := x / int(math.Abs(float64(x)))
 	x = int(math.Abs(float64(x)))
 	tens := int(math.Pow(10, math.Floor(math.Log10(float64(x)))))
